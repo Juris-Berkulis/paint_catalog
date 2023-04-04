9e06e0eb8ec68b48f876f5e20d418d7035303b2e
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-04-04 12:47:43 +0300

    1. Create the "ProductsSort" component and the functionality for its operation and connect this component in the "CatalogView" component (the functionality for sorting products has not yet been created).

---
+++ src/components/ProductsSort.vue
@@ -0,0 +1,98 @@
+<script>
+export default {
+    props: {
+        selectedSort: {
+            type: Object,
+        },
+        sortOptions: {
+            type: Array,
+        },
+        selectSortOption: {
+            type: Function,
+        },
+        isShowSortOptions: {
+            type: Boolean,
+        },
+        setIsShowSortOptions: {
+            type: Function,
+        },
+    },
+    methods: {
+        clickSortOption (sortOption) {
+            this.selectSortOption(sortOption);
+            this.setIsShowSortOptions(false);
+        },
+    },
+}
+</script>
+
+<template>
+<div class="productsSort">
+    <div class="productsSort__Selected">
+        <p class="productsSort__SelectedText" @click="(event) => setIsShowSortOptions(true)">{{ selectedSort.name }}</p>
+    </div>
+    <div class="productsSort__List" v-if="isShowSortOptions">
+        <div class="productsSort__ListItem" v-for="sortOption in sortOptions" :key="sortOptions.name" @click="(event) => clickSortOption(sortOption)">
+            <p class="productsSort__ListItemText">{{ sortOption.name }}</p>
+        </div>
+    </div>
+    <div class="productsSort__closeField" @click="(event) => setIsShowSortOptions(false)" v-if="isShowSortOptions"></div>
+</div>
+</template>
+
+<style scoped>
+.productsSort {
+    position: relative;
+}
+
+.productsSort__Selected {
+    display: flex;
+    cursor: pointer;
+}
+
+.productsSort__SelectedText {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 15px;
+    letter-spacing: 0.06em;
+    text-transform: uppercase;
+}
+
+.productsSort__List {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 248px;
+    background-color: #ffffff;
+    z-index: 2;
+}
+
+.productsSort__ListItem {
+    padding: 16px 24px;
+    background-color: transparent;
+    cursor: pointer;
+}
+
+.productsSort__ListItem:hover {
+    background-color: #7BB899;
+}
+
+.productsSort__ListItemText {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 12px;
+    letter-spacing: 0.06em;
+    text-transform: uppercase;
+}
+
+.productsSort__closeField {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #000000;
+    opacity: 0.7;
+    z-index: 1;
+}
+</style>
--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,15 +1,33 @@
 <script>
+import ProductsSort from '../components/ProductsSort.vue';
 import ProductsList from '../components/productsList.vue';
 import {productsData} from '../data/productsData';
 
 export default {
     components: { 
         ProductsList, 
+        ProductsSort,
     },
     data() {
         return {
             productsData: productsData,
+            selectedSort: {value: 'price', name: 'Сначала дорогие', isReversed: true},
+            sortOptions: [
+                {value: 'price', name: 'Сначала дорогие', isReversed: true},
+                {value: 'price', name: 'Сначала недорогие', isReversed: false},
+                {value: 'popularity', name: 'Сначала популярные', isReversed: true},
+                {value: 'dateAdded', name: 'Сначала новые', isReversed: true},
+            ],
+            isShowSortOptions: false,
         }
+    },
+    methods: {
+        selectSortOption (sortOption) {
+            this.selectedSort = sortOption;
+        },
+        setIsShowSortOptions (boolean) {
+            this.isShowSortOptions = boolean;
+        },
     },
 }
 </script>
@@ -22,7 +40,7 @@ export default {
     <div class="catalogView__main">
         <div class="catalogView__mainHead">
             <p>412 товаров</p>
-            <p>Сначала дорогие</p>
+            <ProductsSort v-bind:selectedSort="selectedSort" v-bind:sortOptions="sortOptions" v-bind:selectSortOption="selectSortOption" v-bind:isShowSortOptions="isShowSortOptions" v-bind:setIsShowSortOptions="setIsShowSortOptions"></ProductsSort>
         </div>
         <ProductsList v-bind:productsData="productsData"></ProductsList>
     </div>
@@ -31,6 +49,11 @@ export default {
 
 <style scoped>
 .catalogView {
+    display: flex;
+    justify-content: space-between;
+}
+
+.catalogView__mainHead {
     display: flex;
     justify-content: space-between;
 }
