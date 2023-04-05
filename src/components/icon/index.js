8e801f0c5e2a6b2606757a7082ecb0b89ff48820
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-04-05 23:11:30 +0300

    1. Create 5 icons components and register them in "src/components/icon/index.js" file.

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,7 +1,17 @@
 import IconArrow from '@/components/icon/IconArrow.vue';
 import IconExpandList from '@/components/icon/IconExpandList.vue';
+import IconCross from '@/components/icon/IconCross.vue';
+import IconLike from '@/components/icon/IconLike.vue';
+import IconProfile from '@/components/icon/IconProfile.vue';
+import IconReturn from '@/components/icon/IconReturn.vue';
+import IconSearch from '@/components/icon/IconSearch.vue';
 
 export const componentsIcon = [
     IconArrow,
     IconExpandList,
+    IconCross,
+    IconLike,
+    IconProfile,
+    IconReturn,
+    IconSearch,
 ];
